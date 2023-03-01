6d4904d99a95938a210f6b778c2ab38ec658d2ed
sai <ojiip95@gmail.com>
2023-03-01 17:41:14 +0200

    adjust rounds() breaks, 5m for normal break and 20m after the 4 rounds

--- timer.sh
+++ timer.sh
@@ -4,15 +4,23 @@
 ## focus rounds function ##
 
 rounds() {
-    for ((round=1; round<4; round++ ))
+    for ((round=1; round<5; round++ ))
     do
         echo you\'r $round begain.
         spd-say "start"
-        sleep 25m
+        sleep 2s # 25m 
 
-        echo take a break for 5 minutes.
-        spd-say "stop"
-        sleep 5m
+        if [ $round -eq 5 ]
+        then
+            echo take a break for 5 minutes.
+            spd-say "stop"
+            sleep 5s # 5m 
+
+        else
+            echo take a break for 20 minutes. 
+            spd-say "Break for 20 mintes"
+            sleep 2s # 20m 
+        fi
 
     done
 
