9ea4d9b9547489839dd1348c3456cb0d4e4b6b57
sai <ojiip95@gmail.com>
2023-03-01 17:32:57 +0200

    more of basic functionality of the script, ask user if for his choice after four sessions, creating a summary() to handle user summary for the sessions.

--- timer.sh
+++ timer.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 
+sessions=0
+
+
 ## focus rounds function ##
 
 rounds() {}
 
+
+
 ## main loop ## 
 
-while [] 
+while [ 0 ] 
 do 
+    if [ $((sessions % 4)) == 0 ]
+    then
+        echo you completed $sessions, do you want to continue? Y or N
+        read user_choice
+        if [ "$user_choice" == "N" || "$user_choice" == "n" ]
+        then
+            # summary()
+            # exit
+        fi 
+    fi
+
+    # rounds
 
 done 
