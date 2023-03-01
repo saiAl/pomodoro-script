92fc1143c3895e7e74b19cfb970e8445d6de4b87
sai <ojiip95@gmail.com>
2023-03-01 17:13:39 +0200

    basic rounds() functionality

--- timer.sh
+++ timer.sh
@@ -3,7 +3,20 @@
 
 ## focus rounds function ##
 
-rounds() {}
+rounds() {
+    for ((round=1; round<4; round++ ))
+    do
+        echo you\'r $round begain.
+        spd-say "start"
+        sleep 25m
+
+        echo take a break for 5 minutes.
+        spd-say "stop"
+        sleep 5m
+
+    done
+
+}
 
 ## main loop ## 
 
